f2b7a3f7a78cf31d68b11e8c74c2acb43b49dd0e
wyjang <wyjang@snu.ac.kr>
2025-04-12 03:14:28 +0900

    assignment

--- test_todo.cpp
+++ test_todo.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "todo.h"
+#include <stdexcept>
 
 TEST(TodoListTest, AddSingleTask) {
     TodoList todo;
@@ -57,6 +58,8 @@ TEST(TodoListTest, RemoveInvalidIndexThrows) {
 
     // TODO: Try removing at invalid indices
     // Example: negative index and out-of-bound index
+    EXPECT_THROW(todo.remove_task(-1), std::invalid_argument);
+    EXPECT_THROW(todo.remove_task(1), std::invalid_argument);
 
     todo.destroy();
 }
@@ -67,6 +70,10 @@ TEST(TodoListTest, AddTooManyTasksThrows) {
 
     // TODO: Add exactly 32 tasks in a loop
     // Then try adding a 33rd task and expect an exception
+    for(int i = 0; i < 32; i++){
+        todo.add_task("Sample Task");
+    }
+    EXPECT_THROW(todo.add_task("Sample Task"), std::overflow_error);
 
     todo.destroy();
 }
--- todo.cpp
+++ todo.cpp
@@ -3,30 +3,62 @@
 
 void TodoList::init() {
     // TODO: set size to 0 and all task pointers to nullptr
+    size = 0;
+    for(int i = 0; i < MAX_TASKS; i++){
+        tasks[i] = nullptr;
+    }
 }
 
 void TodoList::destroy() {
     // TODO: delete all tasks and reset pointers
+    for(int i = 0; i < MAX_TASKS; i++){
+        tasks[i] = nullptr;
+    }
 }
 
 int TodoList::string_length(const char* str) const {
     // TODO: return the number of characters before '\0'
-    return 0;
+    int len = 0;
+    while(str[len] != '\0') len++;
+    return len;
 }
 
 void TodoList::string_copy(char* dest, const char* src) const {
     // TODO: copy characters from src to dest
+    dest = new char[string_length(src) + 1];
+    for(int i = 0; i <= string_length(src); i++){
+        dest[i] = src[i];
+    }
 }
 
 void TodoList::add_task(const char* task) {
     // TODO: if full, throw. otherwise allocate and copy new task
+    if(size == MAX_TASKS) throw std::overflow_error("Todo list is full");
+    tasks[size] = new char[string_length(task) + 1];
+    for(int i = 0; i <= string_length(task); i++){
+        tasks[size][i] = task[i];
+    }
+    size++;
 }
 
 void TodoList::remove_task(int index) {
     // TODO: check bounds, delete task, shift left
+    if(index >= size || index < 0) throw std::invalid_argument("Invalid index");
+    for(int i = index; i < size - 1; i++){
+        delete[] tasks[i];
+        string_copy(tasks[i], tasks[i + 1]);
+    }
+    delete[] tasks[size - 1];
+    size--;
 }
 
 const char** TodoList::get_pending_tasks(int& count) const {
     // TODO: set count and return task array
-    return nullptr;
+    count = size;
+
+    const char** result = new const char*[size+1];
+    for (int i = 0; i < size; i++){
+        result[i] = tasks[i];
+    }
+    return result;
 }
